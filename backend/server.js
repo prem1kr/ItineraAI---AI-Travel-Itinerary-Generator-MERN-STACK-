6d6314bb0b9fb69d50f61b5c8189f2353f4f3546
Prem1kr <prem78334@gmail.com>
2026-06-26 16:33:56 +0530

    server.js correction

--- backend/server.js
+++ backend/server.js
@@ -6,55 +6,26 @@ import authRoutes from "./routes/authRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import itineraryRoutes from "./routes/itineraryRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-
 import errorMiddleware from "./middleware/errorMiddleware.js";
 import { connectDB } from "./config/db.js";
 
 dotenv.config();
 const app = express();
-
 connectDB();
+const PORT = process.env.PORT || 5000;
 
-
-app.use(cors({
-    origin: process.env.CLIENT_URL || "*",
-    credentials: true,
-})
-);
-
+app.use(cors({ origin: process.env.CLIENT_URL || "*", credentials: true }));
 app.use(express.json());
-
-app.use(express.urlencoded({
-    extended: true,
-}));
+app.use(express.urlencoded({ extended: true }));
 
 
-app.use("/uploads", express.static(path.join(
-    process.cwd(), "src/uploads")));
-
-
-
+app.use("/uploads", express.static(path.join(process.cwd(), "src/uploads")));
 app.use("/api/auth", authRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/itinerary", itineraryRoutes);
 app.use("/api/user", userRoutes);
-
-
-
-app.use("*", (req, res) => {
-    res.status(404).json({ success: false, message: "Route Not Found" });
-});
-
-
 app.use(errorMiddleware);
 
-
-const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
-    console.log(`
-🚀 Server Started Successfully
-🌐 Port: ${PORT}
-🗄️ MongoDB Connected
-🤖 Gemini AI Ready`);
+    console.log(` Server Started SuccessfullyPort: ${PORT}MongoDB ConnectedGemini AI Ready`);
 });
